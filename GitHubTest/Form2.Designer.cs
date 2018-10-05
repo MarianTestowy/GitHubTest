75cd121384be0043fe7f0317db79deef336598c3
Adam Filipek <adamfilipek@outlook.com>
2018-10-05 19:39:50 +0200

    sfasfd sdf sdfdsdsf sdfs

--- GitHubTest/Form2.Designer.cs
+++ GitHubTest/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GitHubTest
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(295, 209);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(404, 186);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.Text = "Form2";
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
-
